c3f534d4d4fe58702d011415649b10e526d6eddf
Jun <ljhg1124@gmail.com>
2020-03-10 11:23:08 +0900

    Update app.js
    선 긋기

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 650;
+canvas.height = 650;
+
+ctx.strokeStyle = "#2c2c2c";    // 색상
+ctx.lineWidth = 2.5;            // 라인 굵기
 
 let painting = false;
 
 function stopPainting(){
     painting = false;
 }
+
+function startPainting(){
+    painting = true
+}
+
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x, y);
+    if(!painting){
+        ctx.beginPath();    // 선의 시작
+        ctx.moveTo(x,y);    // 움직이기 시작점
+    } else {
+        ctx.lineTo(x,y);    // 선의 끝점
+        //ctx.closePath();  
+        ctx.stroke();       // 흭 긋기
+        // 마우스가 움직이는 동안 계속 path를 생성하고있다
+    }
 }
 
 function onMouseDown(event){
-    painting = ture;
-}
-
-function onMouseUp(event){
-    stopPainting();
+    painting = true;
 }
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
 
